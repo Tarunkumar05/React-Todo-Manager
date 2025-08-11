366c4f175fb8bf878ac5bd2a2e3972e00d62d1fc
Tarun Kumar Bairwa <101545470+Tarunkumar05@users.noreply.github.com>
2025-08-11 09:08:58 +0530

    Update TodoContexts.js

--- src/contexts/TodoContexts.js
+++ src/contexts/TodoContexts.js
@@ -1,23 +1,21 @@
-import {createContext, useContext} from "react"
+import { createContext, useContext } from 'react';
 
 export const TodoContext = createContext({
-    todos: [
-        {
-            id: 1,
-            todo: "Todo msg",
-            completed: false,
-        }
-    ],
-    addTodo: (todo) => {},
-    updateTodo: (id, todo) => {},
-    deleteTodo: (id) => {},
-    toggleComplete: (id) => {}
-})
+  todos: [],
+  addTodo: () => {},
+  updateTodo: () => {},
+  deleteTodo: () => {},
+  toggleComplete: () => {},
+  filter: 'all',
+  setFilter: () => {},
+  searchTerm: '',
+  setSearchTerm: () => {},
+  sortBy: 'date',
+  setSortBy: () => {},
+});
 
 export const useTodo = () => {
-    return useContext(TodoContext) // jb bhi useContext use krenge isme context dena pdega
-}
+  return useContext(TodoContext);
+};
 
-// ab provider yha se export krte hai vrna main file mai jaake sb kuch wrap krna pdega todocontext.provider
-
-export const TodoProvider = TodoContext.Provider
+export const TodoProvider = TodoContext.Provider;
